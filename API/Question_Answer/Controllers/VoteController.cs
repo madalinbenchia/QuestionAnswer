64d576b162ba3b9a1c18986badf6c07f86728de8
madalinbenchia <33199272+madalinbenchia@users.noreply.github.com>
2021-01-10 16:51:26 +0200

    Add Vote

---
+++ API/Question_Answer/Controllers/VoteController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Question_Answer.Models;
+
+namespace Question_Answer.Controllers
+{
+    public class VoteController : ApiController
+    {
+        private Vote voteObject;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public VoteController()
+        {
+            voteObject = new Vote();
+        }
+
+        /// <summary>
+        /// VoteTypeId - 2(UpVote), 3(DownVote), 5(Favorite)
+        /// </summary>
+        /// <param name="vote"></param>
+        /// <returns></returns>
+        [Route("api/vote/add")]
+        [HttpPost]
+        public HttpResponseMessage AddVote([FromBody]Vote vote)
+        {
+            try
+            {
+                string result = string.Empty;
+                result = voteObject.AddVote(ConfigurationManager.AppSettings["connnectionString"],vote);
+                if (result != string.Empty)
+                    return Request.CreateResponse(System.Net.HttpStatusCode.OK, result);
+                else
+                    return Request.CreateResponse(System.Net.HttpStatusCode.ExpectationFailed, "Vote was not added.");
+            }
+            catch(Exception ex)
+            {
+                log.Error(String.Format("Error adding vote. Error Message: {0} --- StackTrace: {1}", ex.Message, ex.StackTrace));
+                return Request.CreateResponse(System.Net.HttpStatusCode.ExpectationFailed, ex.Message);
+            } 
+        }
+    }
+}
---
+++ API/Question_Answer_DataLayer/Vote.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Question_Answer_DataLayer
+{
+    public class Vote
+    {
+        #region Variables
+        private int postId;
+        private int userId;
+        private int voteTypeId;
+        #endregion
+
+        #region Properties
+        public int PostId { get => postId; set => postId = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public int VoteTypeId { get => voteTypeId; set => voteTypeId = value; }
+        #endregion
+
+        #region Methods
+        public string AddVote(string connectionString, Vote vote)
+        {
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open(); 
+                }
+                catch
+                {
+                    throw new Exception("Unable to establish a connection with the database");
+                }
+                
+                SqlCommand command = new SqlCommand("sp_AddVote", conn);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@PostId", vote.PostId));
+                command.Parameters.Add(new SqlParameter("@UserId", vote.UserId));
+                command.Parameters.Add(new SqlParameter("@VoteType", vote.VoteTypeId));
+                try
+                {
+                    command.ExecuteNonQuery();
+                    return "Success";
+                }catch(Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                
+            }
+        }
+        #endregion
+
+    }
+}
